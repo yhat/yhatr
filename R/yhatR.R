1835bb98b7b8b15ff8349dd5eb47115c412be2b8
ericchiang <eric.chiang.m@gmail.com>
2014-05-07 17:56:39 -0400

    selective object serialization

--- R/yhatR.R
+++ R/yhatR.R
@@ -52,7 +52,7 @@ yhat.post <- function(endpoint, query=c(), data) {
 #'
 check.image.size <- function() {
   total.img.size <- 0
-  for (obj in ls()) {
+  for (obj in yhat.ls()) {
     obj.size <- object.size(get(obj))*9.53674e-7#convert to MB
     total.img.size <- total.img.size + obj.size
     if (obj.size > 20) {
@@ -203,7 +203,7 @@ yhat.deploy <- function(model_name) {
     query <- paste(names(query), query, collapse="&", sep="=")
     url <- paste(url, "deployer/model", "?", query, sep="")
     image_file <- ".yhatdeployment.img"
-    save.image(image_file)
+    save(list=yhat.ls(),file=image_file)
     rsp <- httr::POST(url, httr::authenticate(AUTH["username"], AUTH["apikey"], 'basic'),
          body=list(
            "model_image" = httr::upload_file(image_file),
@@ -293,4 +293,128 @@ model_require <- function() {
   con <- file("yhatExample.R", open="w")
   writeLines(txt, con)
   close(con)
+}
+
+#' Private function for recursively looking for variables
+#'
+#' @param block code block to spider
+#' @param defined.vars variables which have already been defined within the
+#'          scope of the block. e.g. function argument
+yhat.spider.block <- function(block,defined.vars=c()){
+    symbols <- c()
+    n <- length(block)
+    if(n == 0) {
+        return(symbols)
+    }
+    for(i in 1:n){
+        node <- block[[i]]
+        # Really weird bug that comes from assigning the "empty" symbol to a
+        # variable. No obvious way to test for this case other than a try/catch
+        is.valid.symbol <- tryCatch({
+            node
+            TRUE
+        }, error = function(e) {
+            FALSE
+        })
+        if(!is.valid.symbol){ next }
+        node.type <- typeof(node)
+        # if node type is "symbol" then it might be a variable 
+        if(node.type == "symbol"){
+            # if symbol not already defined then it might be a dependency
+            if(!any(node == defined.vars)){
+                symbols <- c(symbols,node)
+            }
+        # if node type is "language" then it is another block we'll want to spider
+        } else if (node.type == "language"){
+            # is the block an assignment statement? if so we'll want to add the
+            # assignment result to the list of defined variables
+            if ((node[[1]] == as.symbol("<-")) || (node[[1]] == as.symbol("="))){
+                # Code will look like this:
+                #     `assign.to` <- `assign.from`
+                assign.from <- node[[3]]
+                assign.from.type <- typeof(assign.from)
+                if (assign.from.type == "symbol"){
+                    # if symbol not already defined then it might be a dependency
+                    if (!any(assign.from == defined.vars)){
+                        symbols <- c(symbols,assign.from)
+                    }
+                } else if (assign.from.type == "language"){
+                    symbols <- c(symbols,yhat.spider.block(assign.from,defined.vars))
+                }
+
+                assign.to <- node[[2]]
+                assign.to.type <- typeof(assign.to)
+                if (assign.to.type == "symbol"){
+                    # yay! the user has defined a variable
+                    defined.vars <- c(assign.to,defined.vars)
+                } else if (assign.to.type == "language"){
+                    # Wait, what?!?! are you assigning to a block of code?
+                    symbols <- c(symbols,yhat.spider.block(assign.from,defined.vars))
+                }
+            } else {
+                # if the block isn't an assignment, recursively crawl
+                symbols <- c(symbols,yhat.spider.block(node,defined.vars))
+            }
+        }
+    }
+    # return a list of symbols which are candidates for global dependency
+    symbols
+}
+
+#' Private function for spidering function source code
+#'
+#' @param func.name name of function you want to spider
+yhat.spider.func <- function(func.name){
+    # parse function to pull out main block and argument names
+    func <- parse(text=getAnywhere(func.name))[[2]][[2]]
+    # we will be comparing symbols not strings
+    args <- lapply(names(func[[2]]),as.symbol)
+    block <- func[[3]]
+    # get all symbols used during function which are dependencies
+    func.vars <- unique(yhat.spider.block(block,defined.vars=args))
+    # return dependency candidates which are defined in the global scope
+    # (these are all variables we'll want to capture)
+    intersect(func.vars,names(as.list(.GlobalEnv)))
+}
+
+#' Private function for determining model dependencies
+#'
+#' List all object names which are dependencies of `model.transform`
+#' and `model.predict`
+yhat.ls <- function(){
+    funcs <- c("model.transform","model.predict") # function queue to spider
+    dependencies <- funcs
+    global.vars <- as.list(.GlobalEnv)
+    while(length(funcs) > 0){
+        # pop first function from queue
+        func.name <- funcs[[1]]
+        n.funcs <- length(funcs)
+        if(n.funcs > 1){
+            funcs <- funcs[2:length(funcs)]
+        } else {
+            funcs <- c()
+        }
+        # spider a function and get all variable dependencies
+        func.vars <- yhat.spider.func(func.name)
+        n.vars <- length(func.vars)
+        if(n.vars > 0){
+            for(i in 1:n.vars){
+                var <- func.vars[[i]]
+                # is variable already a dependency?
+                if(!(var %in% dependencies)){
+                    dependencies <- c(var,dependencies)
+                    # if this variable is a function we're going to
+                    # want to spider it as well
+                    if(typeof(global.vars[[var]]) == "closure"){
+                        # add function to function queue
+                        funcs <- c(var,funcs)
+                    }
+                }
+            }
+        }
+    }
+    if("model.require" %in% names(global.vars)){
+        dependencies <- c("model.require",dependencies)
+    }
+    dependencies
 }
